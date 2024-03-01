b94b598405032ce529b1529b341c27b3a0892419
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-01 14:41:25 -0600

    Fix grid so that it is 16x16

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 let divCount = 1;
-while (divCount <= 16) {
+while (divCount <= 256) {
   const div = document.createElement('div');
   container.appendChild(div);
   divCount++;
