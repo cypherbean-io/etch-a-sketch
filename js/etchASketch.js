87c02aea4acd5debcb9af586292bccbfae888f5c
Eric Helstad <102440393+cypherbean-io@users.noreply.github.com>
2024-03-04 16:14:19 -0600

    Color grid squares when hovered over

--- js/etchASketch.js
+++ js/etchASketch.js
@@ -3,5 +3,8 @@ let divCount = 1;
 while (divCount <= 256) {
   const div = document.createElement('div');
   container.appendChild(div);
+  div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = 'blue';
+  });
   divCount++;
 }
